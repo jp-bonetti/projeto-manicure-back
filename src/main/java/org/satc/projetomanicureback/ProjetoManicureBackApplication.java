0182b7b548b51092b59cb2079ec6eb42ed15b6f4
João Paulo Sigieski Bonetti <joaopaulobonetti2004@gmail.com>
2025-09-19 19:45:09 -0300

    Inicialização do projeto

---
+++ src/main/java/org/satc/projetomanicureback/ProjetoManicureBackApplication.java
@@ -0,0 +1,13 @@
+package org.satc.projetomanicureback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoManicureBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoManicureBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/satc/projetomanicureback/ProjetoManicureBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.satc.projetomanicureback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoManicureBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
