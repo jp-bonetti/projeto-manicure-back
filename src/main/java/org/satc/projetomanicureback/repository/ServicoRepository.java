740e2278d8f3a82810a3de063bc84c89be4aee8d
João Paulo Sigieski Bonetti <joaopaulobonetti2004@gmail.com>
2025-09-20 11:46:51 -0300

    add: repositories

---
+++ src/main/java/org/satc/projetomanicureback/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package org.satc.projetomanicureback.repository;
+
+import org.satc.projetomanicureback.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
